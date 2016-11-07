7ded3bc32c915b8e3887b7a7081e1fc99606fa1a
William Leandro <royalty2012wla@gmail.com>
2016-11-07 01:42:45 -0300

    Se agregan Test sobre coloreo de grafos.

--- Desarrollo/src/test/GrafoTest.java
+++ Desarrollo/src/test/GrafoTest.java
@@ -2,12 +2,54 @@ package test;
 
 import org.junit.Test;
 
-import grafo.MatrizSimetrica;
+import grafo.Coloreo;
+import grafo.Grafo;
 
 public class GrafoTest {
 
 	@Test
-	public void pruebaDeGrafoSimetrico(){
-		MatrizSimetrica simetrica= new MatrizSimetrica(5);
+	public void pruebaDeColoreoAleatorioSecuencialUno(){
+		Grafo grafoUno = new Grafo(PATH_ARCHIVOS + "grafoUno.in");
+		Coloreo testUno;
+		
+		System.out.println("GRAFO 1 :");
+		testUno = new Coloreo(grafoUno.getMatrizSimetrica());
+		testUno.aleatorioSecuencial();
+		testUno.printearColoreo();
 	}
+	
+	@Test
+	public void pruebaDeColoreoAleatorioSecuencialDos(){
+		Grafo grafoDos = new Grafo(PATH_ARCHIVOS + "grafoDos.in");
+		Coloreo testDos;
+		
+		System.out.println("GRAFO 2 :");
+		testDos = new Coloreo(grafoDos.getMatrizSimetrica());
+		testDos.aleatorioSecuencial();
+		testDos.printearColoreo();
+	}
+	
+	@Test
+	public void pruebaDeColoreoWelshPowell(){
+		Grafo grafoTres = new Grafo(PATH_ARCHIVOS + "grafoTres.in");
+		Coloreo testTres;
+		
+		System.out.println("GRAFO 3 :");
+		testTres = new Coloreo(grafoTres.getMatrizSimetrica());
+		testTres.resolucionWelshPowell();
+		testTres.printearColoreo();
+	}
+	
+	@Test
+	public void pruebaDeColoreoMatula(){
+		Grafo grafoTres = new Grafo(PATH_ARCHIVOS + "grafoTres.in");
+		Coloreo testTres;
+		
+		System.out.println("GRAFO 4 :");
+		testTres = new Coloreo(grafoTres.getMatrizSimetrica());
+		testTres.resolucionMatula();
+		testTres.printearColoreo();
+	}
+	
+	private static final String PATH_ARCHIVOS = "PreparacionDePrueba/LoteDePrueba/IN/";
 }

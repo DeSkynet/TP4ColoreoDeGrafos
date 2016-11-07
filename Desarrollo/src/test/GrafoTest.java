eb0360a0811dfd36870292a7eb4701a7760e7a6f
William Leandro <royalty2012wla@gmail.com>
2016-11-07 12:41:46 -0300

    Cambios Manores.

--- Desarrollo/src/test/GrafoTest.java
+++ Desarrollo/src/test/GrafoTest.java
@@ -3,52 +3,44 @@ package test;
 import org.junit.Test;
 
 import grafo.Coloreo;
-import grafo.Grafo;
+import grafo.GrafoNDNP;
 
 public class GrafoTest {
 
 	@Test
 	public void pruebaDeColoreoAleatorioSecuencialUno(){
-		Grafo grafoUno = new Grafo(PATH_ARCHIVOS + "grafoUno.in");
-		Coloreo testUno;
+		GrafoNDNP grafoUno = new GrafoNDNP(PATH_ARCHIVOS + "grafoUno.in");
 		
 		System.out.println("GRAFO 1 :");
-		testUno = new Coloreo(grafoUno.getMatrizSimetrica());
-		testUno.aleatorioSecuencial();
-		testUno.printearColoreo();
+		grafoUno.getColoreo().aleatorioSecuencial();
+		grafoUno.getColoreo().printearColoreo();
 	}
 	
 	@Test
 	public void pruebaDeColoreoAleatorioSecuencialDos(){
-		Grafo grafoDos = new Grafo(PATH_ARCHIVOS + "grafoDos.in");
-		Coloreo testDos;
+		GrafoNDNP grafoDos = new GrafoNDNP(PATH_ARCHIVOS + "grafoDos.in");
 		
 		System.out.println("GRAFO 2 :");
-		testDos = new Coloreo(grafoDos.getMatrizSimetrica());
-		testDos.aleatorioSecuencial();
-		testDos.printearColoreo();
+		grafoDos.getColoreo().aleatorioSecuencial();
+		grafoDos.getColoreo().printearColoreo();
 	}
 	
 	@Test
 	public void pruebaDeColoreoWelshPowell(){
-		Grafo grafoTres = new Grafo(PATH_ARCHIVOS + "grafoTres.in");
-		Coloreo testTres;
+		GrafoNDNP grafoTres = new GrafoNDNP(PATH_ARCHIVOS + "grafoTres.in");
 		
 		System.out.println("GRAFO 3 :");
-		testTres = new Coloreo(grafoTres.getMatrizSimetrica());
-		testTres.resolucionWelshPowell();
-		testTres.printearColoreo();
+		grafoTres.getColoreo().resolucionWelshPowell();
+		grafoTres.getColoreo().printearColoreo();
 	}
 	
 	@Test
 	public void pruebaDeColoreoMatula(){
-		Grafo grafoTres = new Grafo(PATH_ARCHIVOS + "grafoTres.in");
-		Coloreo testTres;
+		GrafoNDNP grafoTres = new GrafoNDNP(PATH_ARCHIVOS + "grafoTres.in");
 		
 		System.out.println("GRAFO 4 :");
-		testTres = new Coloreo(grafoTres.getMatrizSimetrica());
-		testTres.resolucionMatula();
-		testTres.printearColoreo();
+		grafoTres.getColoreo().resolucionMatula();
+		grafoTres.getColoreo().printearColoreo();
 	}
 	
 	private static final String PATH_ARCHIVOS = "PreparacionDePrueba/LoteDePrueba/IN/";
